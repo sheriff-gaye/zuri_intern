7527925b0355e3ed66a8b06beb43f8d0345c0230
sheriff-gaye <sheriffgaye5@gmail.com>
2022-05-26 13:06:21 +0000

    zuri

--- main.py
+++ main.py
@@ -1,38 +1,20 @@
+# Check if two words are anagrams 
+# Example:
+# find_anagrams("hello", "check") --> False
+# find_anagrams("below", "elbow") --> True
 
-class Student:
-    # [assignment] Skeleton class. Add your code here
-    def __init__(self,name,age,tracks,score):
-        self.name=name
-        self.age=age
-        self.tracks=tracks
-        self.score=score
 
-    #function to change_name
-    def change_name(self,new_name):
-        self.name=new_name
+def find_anagram(word, anagram):
+    # [assignment] Add your code here
+
+    if sorted (word)== sorted(anagram):
+        print("True")
     
-    #function to change_age
-    def change_age(self,newage):
-        self.age=newage
-    
-    #function to add new_tracks
-    def add_track(self, new_track):
-        self.tracks.append(new_track)
-    
-    #function to get_scores
-    def get_score(self):
-        return (self.score)
+    else:
+        print("False")
 
-        
-
-
-
-
-Bob = Student(name="Bob", age=26, tracks=["FE","BE"],score=20.90)
-
-# Expected methods
-Bob.change_name("Peter")
-Bob.change_age(34)
-Bob.add_track("UI/UX")
-Bob.get_score()
+#calling function with samples
+find_anagram('heart','earth')
+find_anagram("left",'felt')
+find_anagram("hello","ello")
 
